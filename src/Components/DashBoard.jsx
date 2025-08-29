d61a09c255565166d60bf42365705ba45efab013
Opeyemi Esan <tolulopejossyp@gmail.com>
2025-08-29 15:07:26 +0100

    second commit

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DashBoardHeader } from './DashBoardHeader';
 import { UserList } from '../Components/UserList';
+import { Link } from 'react-router-dom';
 
 export const DashBoard = () => {
 
@@ -8,6 +9,9 @@ export const DashBoard = () => {
   return (
     <div className='h-screen'>
       <DashBoardHeader />
+      <div className='flex justify-center items-center h-[calc(100vh-80px)] bg-slate-100'>
+        <Link to="/users" className='bg-slate-400  text-center px-60 py-30 text-4xl text-slate-50 font-semibold hover:bg-slate-500 cursor-pointer drop-shadow-lg transition-all duration-300 ease-in-out'>View Users</Link>
+      </div>
     </div>
   )
 }
--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -3,6 +3,9 @@ import api from "../Auth/api";
 import { AuthService } from "../Auth/AuthService";
 import { Link } from "react-router-dom";
 import { BiEdit } from "react-icons/bi";
+import { FiDelete } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
 
 export const UserList = () => {
 
@@ -42,6 +45,29 @@ export const UserList = () => {
     fetchUsers();
   }, []);
 
+
+const deleteUser = async (email) => {
+  try {
+    const token = AuthService.getToken();
+
+    const response = await axios.delete(
+      `https://localhost:7221/api/User/delete_user/${encodeURIComponent(email)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    window.location.reload();
+    alert("User deleted successfully!");
+    console.log("User deleted:", response.data);
+  } catch (error) {
+    setError(error.response?.data?.message || "An error occurred");
+    alert("Failed to delete user.");
+    console.error("Error deleting user:", error.response?.data || error.message);
+  }
+};
+
   return (
     <div className="m-h-screen p-10 px-20">
       <h1 className="text-2xl font-bold mb-4">User List</h1>
@@ -53,12 +79,15 @@ export const UserList = () => {
 
           {!loading && users.map((user, index) => (
               <div key={index} className="mb-3 p-3 bg-gray-200 rounded-lg flex justify-between items-center px-10 py-10">
-                <div>
-                  <h2 className="font-semibold text-2xl">{user.firstName} {user.lastName}</h2>
-                  <p className="text-gray-700">{user.email}</p>
-                  <p className="bg-slate-500 px-4 py-2 rounded text-center text-slate-100">{user.nationality}</p>
-                  <img src={user.profilePicture}  alt=""/>
-                </div>
+                  <div className="flex items-center gap-10">
+                      <div>
+                          <h2 className="font-semibold text-2xl">{user.firstName} {user.lastName}</h2>
+                          <p className="text-gray-700">{user.email}</p>
+                          <p className="bg-slate-500 px-4 py-2 rounded text-center text-slate-100">{user.nationality}</p>
+                      </div>
+                      <img className="w-40" src={user.profilePicture} alt="" />
+                      <Link onClick={() => deleteUser(user.email)} className="bg-red-500 px-5 py-2 text-white font-semibold"><MdDelete /></Link>
+                  </div>
 
                 <Link className="font-bold text-2xl" to="/updateuser"><BiEdit /></Link>
               </div>
